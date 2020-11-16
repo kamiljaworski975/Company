52c87450d7282c29e7eb8e9068fc44a9aadbbb38
Kamil Jaworski <jaworskiebiznes@gmail.com>
2020-11-16 09:26:56 +0000

    add validation form contact

--- Project/scripts/form-step.js
+++ Project/scripts/form-step.js
@@ -4,9 +4,16 @@
     const buttonSecond = document.getElementById("form-next-2");
     const backFirst = document.getElementById("form-back-1");
     const backSecond = document.getElementById("form-back-2");
+    const buttonSend = document.getElementById("form-send");
     const step1 = document.querySelector(".form__field--step-1");
     const step2 = document.querySelector(".form__field--step-2");
     const step3 = document.querySelector(".form__field--step-3");
+    const errorStepOne = document.getElementById("error-step--one");
+    const budgetError = document.getElementById("budget-error");
+    const startError = document.getElementById("start-error");
+    const firstNameError = document.getElementById("firstName-error");
+    const lastNameError = document.getElementById("lastName-error");
+    const emailError = document.getElementById("email-error");
     const checkboxes = Array.from(
       document.querySelectorAll(".form__field--checkbox")
     );
@@ -27,19 +34,46 @@
 
     const checkStepTwo = (obj) => {
       const check = obj["Budget"] && obj["Start"];
+
+      if (obj["Start"] == false) {
+        startError.style.display = "block";
+      } else {
+        startError.style.display = "none";
+      }
+      if (obj["Budget"] == false) {
+        budgetError.style.display = "block";
+      } else {
+        budgetError.style.display = "none";
+      }
       return check;
     };
 
-    // const checkStepThree = (obj) => {
-    //     const check = obj
-    // }
+    const checkStepThree = (obj) => {
+      const check = obj["FirstName"] && obj["LastName"] && obj["Email"];
+
+      if (obj["FirstName"] == false) {
+        firstNameError.style.display = "block";
+      } else {
+        firstNameError.style.display = "none";
+      }
+      if (obj["LastName"] == false) {
+        lastNameError.style.display = "block";
+      } else {
+        lastNameError.style.display = "none";
+      }
+      if (obj["Email"] == false) {
+        emailError.style.display = "block";
+      } else {
+        emailError.style.display = "none";
+      }
+      return check;
+    };
 
     const handleOnChange = (el, obj, checkbox) => {
       el.addEventListener("change", (e) => {
         const name = e.target.name;
         const value = checkbox ? e.target.checked : e.target.value;
         obj[name] = value;
-        console.log(obj);
       });
     };
 
@@ -94,13 +128,14 @@
       if (!checkStep(formState.stepOne)) {
         step2.style.animation = "none";
         step1.style.animation = "fade-out-left 1s ease-in both";
+        errorStepOne.style.display = "none";
         setTimeout(() => {
           step2.style.display = "block";
           step1.style.display = "none";
           window.scrollTo(0, 0);
         }, 500);
       } else {
-        console.log("musisz wypełnic pole");
+        errorStepOne.style.display = "block";
       }
     });
 
@@ -116,6 +151,8 @@
 
     buttonSecond.addEventListener("click", () => {
       if (checkStepTwo(formState.stepTwo)) {
+        startError.style.display = "none";
+        budgetError.style.display = "none";
         step3.style.animation = "none";
         step2.style.animation = "fade-out-left 1s ease-in both";
         setTimeout(() => {
@@ -124,7 +161,7 @@
           window.scrollTo(0, 0);
         }, 500);
       } else {
-        console.log("step 2 error");
+        return false;
       }
     });
 
@@ -136,6 +173,24 @@
         step3.style.display = "none";
         window.scrollTo(0, 0);
       }, 500);
+    });
+
+    buttonSend.addEventListener("click", () => {
+      const dataBase = [];
+      if (checkStepThree(formState.stepThree)) {
+        console.log("idz dalej");
+        dataBase.push(
+          formState.stepOne,
+          formState.stepTwo,
+          formState.stepThree
+        );
+        console.log(dataBase);
+        firstNameError.style.display = "none";
+        lastNameError.style.display = "none";
+        emailError.style.display = "none";
+      } else {
+        console.log("brakuje");
+      }
     });
   });
 })();
